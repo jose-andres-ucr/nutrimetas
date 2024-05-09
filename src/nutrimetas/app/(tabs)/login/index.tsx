e519ba7b053c948400f933df45cecdd8954f2dc0
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-05-08 20:30:20 -0600

    Added barebones login query

--- src/nutrimetas/app/(tabs)/login/index.tsx
+++ src/nutrimetas/app/(tabs)/login/index.tsx
@@ -1,6 +1,9 @@
 "use strict"
 
 // Dependencies
+// Core React hooks & misc. stuff
+import { useState, useEffect } from "react";
+
 // Core React Native UI
 import { View, Text, StyleSheet, ImageSourcePropType } from "react-native";
 
@@ -11,9 +14,115 @@ import { Image } from "expo-image";
 // Login form 
 import LoginForm from "@/components/LoginForm";
 
+// Firestore DB
+import firestore from '@react-native-firebase/firestore';
+
+/// Credential verification
+const checkAuth = function(email: string, password: string, 
+    credentials : any)
+{
+    if (credentials.email == email &&
+        credentials.password == password)
+    {console.log("Credential valid");}
+    else
+    {console.log("Credential invalid");}
+
+    return (
+        credentials.email === email &&
+        credentials.password === password
+    );
+}
+
+// Possible login states 
+type LoginStatus = "pending" | "invalid" | "signed-in" | "signed-out";
+
 // Login form rendering and hooks
 export default function LoginPage(
 ) {
+    // Keep track of current login state
+    const [loginState, setLoginState] = useState("pending" as LoginStatus);
+
+    // Assume initial login state is to be signed out
+    useEffect(() => {setLoginState("signed-out");}, []);
+
+    // Login handling
+    const handleLogin = async function(
+        data : {email: string, password: string}
+    )
+    {
+        // Keep track of... 
+
+        // ... the current state of login
+        setLoginState("pending");
+        
+        // ...and unexpected DB errors
+        let unexpectedError = null;
+
+        // Find the proper person associated with said credentials
+        let person = null;
+
+        // Be it a patient...
+        await firestore()
+            .collection("Patient")
+            .where("email", "==", data.email)
+            .limit(1)
+            .get()
+            .then(
+                (QuerySnapshot) => {
+                    if (QuerySnapshot.size > 0)
+                    { person = QuerySnapshot.docs[0].data(); }
+                },
+                (Error) => {
+                    unexpectedError = Error;
+                    console.error(
+                        "Unexpected error while authenticating against \
+                        patients",
+                        Error
+                    )
+                }
+            );
+
+        // Or a professional
+        person ?? await firestore()
+            .collection("Professionals")
+            .where("email", "==", data.email)
+            .limit(1)
+            .get()
+            .then(
+                (QuerySnapshot) => {
+                    if (QuerySnapshot.size > 0)
+                    { person = QuerySnapshot.docs[0].data(); }
+                },
+                (Error) => {
+                    unexpectedError = Error;
+                    console.error(
+                        "Unexpected error while authenticating against \
+                        professionals",
+                        Error
+                    )
+                }
+            );
+
+        // If an associated person is found and its credentials are
+        // sucesfully validated, accept the login and pass its credentials
+        // along with the user control to the next screens
+        if (
+            unexpectedError == null && 
+            person != null && 
+            checkAuth(data.email, data.password, person)
+        )
+        {
+            console.log("Succesful login!");
+            setLoginState("signed-in");
+        }
+    
+        // Otherwise, reject the login
+        else {
+            console.log("Failed login :(");
+            setLoginState("invalid");
+        }
+    }
+
     // Register the icon loading hook
     const [icon, error] = useAssets([
         require('@/assets/images/logo.png')
@@ -40,8 +149,7 @@ export default function LoginPage(
             { /* Login form */ }
             <View style={LoginStyles.FormView}>
                 <LoginForm 
-                    onSubmit={({email, password}) => 
-                    console.log("Form data submitted:", email, password)}
+                    onSubmit={handleLogin}
                 />
             </View>
         </View>
