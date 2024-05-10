d28d8a6bb1d89e8ed999001a52870f13f6dd5b7f
lizethcorrales <lizeth.corrales@ucr.ac.cr>
2024-05-09 21:50:58 -0600

    delete two screen

--- src/nutrimetas/app/(tabs)/_layout.tsx
+++ src/nutrimetas/app/(tabs)/_layout.tsx
@@ -34,13 +34,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="goals"
         options={{
           title: 'Goals',
