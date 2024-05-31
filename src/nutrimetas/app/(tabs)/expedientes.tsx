267f7083db0541087ecd596ed6a1e8da56a491bc
EysonJaffet <eyson.alfaro@ucr.ac.cr>
2024-05-30 18:11:47 -0600

    Se arregla un error en el estilo

--- src/nutrimetas/app/(tabs)/expedientes.tsx
+++ src/nutrimetas/app/(tabs)/expedientes.tsx
@@ -21,6 +21,7 @@ export default function Expedientes() {
 const styles = StyleSheet.create({
   container: {
     height: '100%',
+    backgroundColor: Colors.lightblue,
   },
   listContainer: {
     backgroundColor: Colors.white,
