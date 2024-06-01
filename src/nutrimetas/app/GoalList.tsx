70db34ad173401671cf0132f23e1f1a45d50e6f0
ChristopherObando <69659737+ChristopherObando@users.noreply.github.com>
2024-06-01 12:11:01 -0600

    Bug fixes when merging with develop

--- src/nutrimetas/app/GoalList.tsx
+++ src/nutrimetas/app/GoalList.tsx
@@ -1,39 +1,40 @@
-
 import { StyleSheet, TouchableOpacity, FlatList, View, Text, Image } from 'react-native';
 import React, { useState, useEffect, useContext } from 'react';
 import firestore from '@react-native-firebase/firestore';
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { router, useRouter, useSearchParams } from 'expo-router';
+import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { SessionContext } from '@/shared/LoginSession';  // Importa el contexto de la sesión
 import { useGlobalSearchParams } from 'expo-router';
 
 const GoalList = () => {
-    // const route = useRoute();
     const navigation = useNavigation();
     const { patientId } = useGlobalSearchParams();
     const { role } = useContext(SessionContext);
-    // const patientId = route.params?.sessionDocId;
     const [goals, setGoals] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
-    // console.log(patientId);
+
     useEffect(() => {
-        const unsubscribe = firestore()
-            .collection('Patient')
-            .doc(patientId.toString())
-            .onSnapshot((snapshot) => {
-                const patientData = snapshot.data();
-                const patientGoals = patientData && patientData.Goals ? patientData.Goals : [];
+        if (patientId) {
+            const unsubscribe = firestore()
+                .collection('Patient')
+                .doc(patientId.toString())
+                .onSnapshot((snapshot) => {
+                    const patientData = snapshot.data();
+                    const patientGoals = patientData && patientData.Goals ? patientData.Goals : [];
 
-                if (patientGoals.length > 0) {
-                    fetchGoalsFromFirebase(patientGoals);
-                } else {
-                    setLoading(false); // No hay metas, actualiza el estado de carga
-                    console.log('El paciente no tiene metas.');
-                }
-            });
+                    if (patientGoals.length > 0) {
+                        fetchGoalsFromFirebase(patientGoals);
+                    } else {
+                        setLoading(false); // No hay metas, actualiza el estado de carga
+                        console.log('El paciente no tiene metas.');
+                    }
+                });
 
-        return () => unsubscribe();
+            return () => unsubscribe();
+        } else {
+            // console.error('patientId is undefined');
+            setLoading(false);
+        }
     }, [patientId]);
 
     const fetchGoalsFromFirebase = async (patientGoals: any) => {
@@ -44,16 +45,14 @@ const GoalList = () => {
             if (typeof goalId.id === 'string') {
                 const goalDoc = await firestore().collection('Goal').doc(goalId.id).get();
                 if (goalDoc.exists) {
-                    // console.log(goalDoc.data());
                     const goalData = goalDoc.data();
-                    if (goalData) { // Verificar si goalData está definido
+                    if (goalData) {
                         const title = await buildTitle(goalData.Rubric);
                         const description = await buildDescription(goalData);
                         goalsFromFirebase.push({ ...goalData, title, description });
                     } else {
                         console.error('Goal data is undefined for goal ID:', goalId.id);
                     }
-                    // goalsFromFirebase.push(goalDoc.data());
                 }
             } else {
                 console.error('Invalid goal ID:', goalId);
@@ -117,7 +116,6 @@ const GoalList = () => {
         }
     };
 
-
     const onPressHandle = (selectedGoal: any) => {
         console.log(selectedGoal);
     };
@@ -129,7 +127,7 @@ const GoalList = () => {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <TouchableOpacity onPress={() => router.back()}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Icon name="arrow-back" size={24} color="black" />
                 </TouchableOpacity>
                 <Text style={styles.title}>Metas</Text>
@@ -157,7 +155,7 @@ const GoalList = () => {
                             </View>
                         </TouchableOpacity>
                     )}
-                    keyExtractor={(item, index) => index.toString()} // TODO: Utilizar llave primaria de BD
+                    keyExtractor={(item, index) => `${item.title}-${index}`} // Aseguramos un key único
                 />
             )}
             {role === 'professional' && (
@@ -232,125 +230,3 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
 });
-
-
-/*
-import { StyleSheet, TouchableOpacity, FlatList, View, Text, Image } from 'react-native';
-import React, { useState, useEffect } from 'react';
-import firestore from '@react-native-firebase/firestore';
-import { useRoute } from '@react-navigation/native';
-
-const GoalList = () => {
-    const route = useRoute();
-    const patientId = route.params?.selectedPatientId;
-    const [goals, setGoals] = useState<any[]>([]);
-    //const [patientInfo, setPatientInfo] = useState<any>();
-
-    useEffect(() => {
-        const patient = firestore()
-            .collection('Patient')
-            .where('name', '==', patientId);
-        const patientUnsubscribe = patient.onSnapshot((snapshot) => {
-            const patientData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-            console.log(patientData[0].Goals[0]);
-            //setPatientInfo(patientData);
-
-            // Solo obtendremos el primer objetivo del primer paciente para establecerlo
-            if (patientData.length > 0 && patientData[0].Goals.length > 0) {
-                patientData.forEach(patient => {
-                    patient.Goals.forEach((goal, index) => {
-                        // Esto imprimirá cada objetivo del paciente
-                        const goalRef = patient.Goals[index];
-                        //console.log(patient.Goals[index]);
-                        goalRef.get().then((doc) => {
-                            if (doc.exists) {
-                                setGoals([doc.data()]);
-                            } else {
-                                console.log("No such document!");
-                            }
-                        }).catch((error) => {
-                            console.log("Error getting document:", error);
-                        });
-                    });
-                });
-            } else {
-                console.log("The patient does have goals");
-            }
-        });
-        return () => {
-            patientUnsubscribe();
-        };
-    }, [patientId]);
-
-
-    const getDescription = (goal: any) => {
-        return `${goal.Description} a ${goal.Frequency} veces, ${goal.Modality}`;
-    };
-
-    const onPressHandle = (selectedGoal: any) => {
-        console.log(selectedGoal);
-    };
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Metas</Text>
-            <FlatList
-                data={goals}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => onPressHandle(item)}>
-                        <View style={styles.item}>
-                            <Image
-                                style={styles.itemImage}
-                                source={{ uri: 'https://icons-for-free.com/iff/png/256/profile+profile+page+user+icon-1320186864367220794.png' }}
-                            />
-                            <View style={styles.goalDetails}>
-                                <Text style={styles.itemTitle}>{item.Title}</Text>
-                                <Text style={styles.itemDescription}>{getDescription(item)}</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                )}
-                keyExtractor={(item) => item.id}
-            />
-        </View>
-    );
-}
-
-export default GoalList;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 50,
-        paddingLeft: 20,
-    },
-    title: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        textAlign: 'left',
-        marginBottom: 10,
-    },
-    item: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        margin: '2%',
-        borderBottomWidth: 1
-    },
-    itemImage: {
-        width: 60,
-        height: 60,
-        marginRight: 10
-    },
-    goalDetails: {
-        flex: 1
-    },
-    itemTitle: {
-        fontWeight: 'bold',
-        fontSize: 16
-    },
-    itemDescription: {
-        fontSize: 14
-    }
-});
-
-*/
