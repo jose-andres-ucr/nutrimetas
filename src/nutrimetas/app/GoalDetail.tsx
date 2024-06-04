3c8bbeb6f674512a8d52de70e6aeb80ccb202ce7
ChristopherObando <69659737+ChristopherObando@users.noreply.github.com>
2024-06-04 11:48:38 -0600

    Update GoalDetail.tsx
    The details of the goal are displayed correctly on the screen

--- src/nutrimetas/app/GoalDetail.tsx
+++ src/nutrimetas/app/GoalDetail.tsx
@@ -1,17 +1,41 @@
-import { StyleSheet, TouchableOpacity, View, Text, Image } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import { StyleSheet, TouchableOpacity, View, Text, ActivityIndicator } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useGlobalSearchParams } from 'expo-router';
+import Colors from '@/constants/Colors';
 
-// Define the Timestamp interface
 interface Timestamp {
     seconds: number;
     nanoseconds: number;
 }
 
-// Define the GoalData interface
+interface TypeData {
+    Name: string;
+}
+
+interface ActionData {
+    Name: string;
+}
+
+interface RubricData {
+    Name: string;
+}
+
+interface AmountData {
+    Value: string;
+}
+
+interface FrequencyData {
+    Name: string;
+}
+
+interface PortionData {
+    Name: string;
+    Plural: string;
+}
+
 interface GoalData {
     Action: string;
     Amount: string;
@@ -24,6 +48,12 @@ interface GoalData {
     Type: string;
     title?: string;
     description?: string;
+    TypeData?: TypeData;
+    ActionData?: ActionData;
+    RubricData?: RubricData;
+    AmountData?: AmountData;
+    FrequencyData?: FrequencyData;
+    PortionData?: PortionData;
 }
 
 const GoalDetail = () => {
@@ -33,36 +63,68 @@ const GoalDetail = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        if (selectedGoal) {
-            const unsubscribe = firestore()
-                .collection('Goal')
-                .doc(selectedGoal.toString())
-                .onSnapshot((snapshot) => {
-                    const goalDoc = snapshot.data();
+        const fetchGoalDetails = async () => {
+            if (selectedGoal) {
+                try {
+                    const goalSnapshot = await firestore().collection('Goal').doc(selectedGoal.toString()).get();
+                    const goalDoc = goalSnapshot.data();
                     if (goalDoc) {
-                        console.log(goalDoc);
-                        setGoalData(goalDoc as GoalData);
-                        setLoading(false);
+                        const typePromise = firestore().collection('Type').doc(goalDoc.Type).get();
+                        const actionPromise = firestore().collection('Action').doc(goalDoc.Action).get();
+                        const rubricPromise = firestore().collection('Rubric').doc(goalDoc.Rubric).get();
+                        const amountPromise = firestore().collection('Amount').doc(goalDoc.Amount).get();
+                        const frequencyPromise = firestore().collection('Frequency').doc(goalDoc.Frequency).get();
+                        const portionPromise = firestore().collection('Portion').doc(goalDoc.Portion).get();
+
+                        const [typeData, actionData, rubricData, amountData, frequencyData, portionData] = await Promise.all([
+                            typePromise,
+                            actionPromise,
+                            rubricPromise,
+                            amountPromise,
+                            frequencyPromise,
+                            portionPromise
+                        ]);
+
+                        setGoalData({
+                            ...goalDoc,
+                            TypeData: typeData.data(),
+                            ActionData: actionData.data(),
+                            RubricData: rubricData.data(),
+                            AmountData: amountData.data(),
+                            FrequencyData: frequencyData.data(),
+                            PortionData: portionData.data()
+                        } as GoalData);
                     } else {
                         console.error('No such document!');
                     }
+                } catch (error) {
+                    console.error('Error fetching goal details:', error);
+                } finally {
+                    setLoading(false);
+                }
+            } else {
+                setLoading(false);
+            }
+        };
 
-                });
-
-            return () => unsubscribe();
-        } else {
-            // console.error('patientId is undefined');
-            setLoading(false);
-        }
-    }, []);
+        fetchGoalDetails();
+    }, [selectedGoal]);
 
     if (loading) {
-        return <Text>Cargando...</Text>;
+        return (
+            <View style={styles.loadingContainer}>
+                <ActivityIndicator size="large" color={Colors.blue} />
+                <Text style={styles.loadingText}>Cargando...</Text>
+            </View>
+        );
     }
 
     if (!goalData) {
         return <Text>No se encontraron detalles para esta meta.</Text>;
     }
+
+    const { TypeData, ActionData, RubricData, AmountData, FrequencyData, PortionData } = goalData;
+    const portionText = AmountData?.Value === '1' ? PortionData?.Name : PortionData?.Plural;
 
     return (
         <View style={styles.container}>
@@ -73,23 +135,18 @@ const GoalDetail = () => {
                 <Text style={styles.title}>Detalles</Text>
             </View>
             <View style={styles.detailContainer}>
-                <Text style={styles.detailText}>Tipo: {goalData.title}</Text>
-                <Text style={styles.detailText}>Cantidad: {goalData.description}</Text>
+                <Text style={styles.detailText}>
+                    <Text style={styles.boldText}>Tipo: </Text>
+                    {TypeData?.Name} {ActionData?.Name} {RubricData?.Name}
+                </Text>
+                <Text style={styles.detailText}>
+                    <Text style={styles.boldText}>Cantidad: </Text>
+                    {AmountData?.Value} {portionText} {FrequencyData?.Name}
+                </Text>
             </View>
         </View>
     );
-}
-
-/*              <Text style={styles.detailText}>Acción: {goalData.Action}</Text>
-                <Text style={styles.detailText}>Cantidad: {goalData.Amount}</Text>
-                <Text style={styles.detailText}>Fecha Límite: {new Date(goalData.Deadline.seconds * 1000).toLocaleDateString()}</Text>
-                <Text style={styles.detailText}>Frecuencia: {goalData.Frequency}</Text>
-                <Text style={styles.detailText}>Hora de Notificación: {new Date(goalData.NotificationTime.seconds * 1000).toLocaleTimeString()}</Text>
-                <Text style={styles.detailText}>Porción: {goalData.Portion}</Text>
-                <Text style={styles.detailText}>Rúbrica: {goalData.Rubric}</Text>
-                <Text style={styles.detailText}>Fecha de Inicio: {new Date(goalData.StartDate.seconds * 1000).toLocaleDateString()}</Text>
-                <Text style={styles.detailText}>Tipo: {goalData.Type}</Text>
-*/
+};
 
 export default GoalDetail;
 
@@ -118,7 +175,18 @@ const styles = StyleSheet.create({
     },
     detailText: {
         fontSize: 17,
-        fontWeight: 'bold',
         marginVertical: 25,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loadingText: {
+        marginTop: 10,
+        fontSize: 18,
     },
 });
