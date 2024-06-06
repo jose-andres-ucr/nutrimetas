03b7b4803aaa29e728b86691864bdd8ae82e70b6
Gian <gfsopa@outlook.com>
2024-06-06 14:09:50 -0600

    Cleaning code

--- src/nutrimetas/app/GoalList.tsx
+++ src/nutrimetas/app/GoalList.tsx
@@ -18,31 +18,25 @@ const GoalList = () => {
     const { role } = useContext(SessionContext);
     const [goals, setGoals] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
-
-    const [showCalendar, setShowCalendar] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
-    const [selectedOption, setSelectedOption] = useState("");
 
     // estados para las fechas
     const [showStartDatePicker, setShowStartDatePicker] = useState(false);
     const [startDate, setStartDate] = useState(new Date());
-
     const [originalGoals, setOriginalGoals] = useState<any[]>([]);
-    const [originalGoalsSaved, setOriginalGoalsSaved] = useState(false); // Variable para controlar si se han guardado los datos originales
-    const [showBackdrop, setShowBackdrop] = useState(false); //oscurece la pantalla 
+    const [originalGoalsSaved, setOriginalGoalsSaved] = useState(false);
+    const [showBackdrop, setShowBackdrop] = useState(false);
+
     useEffect(() => {
         // Guarda las metas originales solo la primera vez que se cargan
-        if (!originalGoalsSaved) {
+        if (!originalGoalsSaved && goals.length > 0) {
             setOriginalGoals(goals);
             setOriginalGoalsSaved(true);
         }
     }, [goals, originalGoalsSaved]);
 
     useEffect(() => {
-        setOriginalGoalsSaved(false);
-    }, [patientId]);
-
-    useEffect(() => {
+        // Maneja la suscripción a los cambios de Patient en Firestore
         if (patientId) {
             const unsubscribe = firestore()
                 .collection('Patient')
@@ -50,7 +44,7 @@ const GoalList = () => {
                 .onSnapshot((snapshot) => {
                     const patientData = snapshot.data();
                     const patientGoals = patientData && patientData.Goals ? patientData.Goals : [];
-                    
+
                     if (patientGoals.length > 0) {
                         fetchGoalsFromFirebase(patientGoals);
                     } else {
@@ -58,7 +52,6 @@ const GoalList = () => {
                         console.log('El paciente no tiene metas.');
                     }
                 });
-
             return () => unsubscribe();
         } else {
             setLoading(false);
@@ -71,7 +64,7 @@ const GoalList = () => {
         for (const goalId of patientGoals) {
             if (typeof goalId.id === 'string') {
                 const goalDoc = await firestore().collection('Goal').doc(goalId.id).get();
-                console.log('Datos de la meta:', goalDoc.data());
+                //console.log('Datos de la meta:', goalDoc.data());
                 const goalSelectId = goalDoc.id;
                 if (goalDoc.exists) {
                     const goalData = goalDoc.data();
@@ -115,7 +108,6 @@ const GoalList = () => {
     const fetchReferenceData = async (collection: string, docId: string) => {
         const doc = await firestore().collection(collection).doc(docId).get();
         const data = doc.exists ? doc.data() : null;
-        console.log('Datos obtenidos de Firebase:', data); // Imprime los datos obtenidos de Firebase
         return data;
     };
     
@@ -152,7 +144,7 @@ const GoalList = () => {
     };
 
     const onPressHandle = (selectedGoalId: string) => {
-        console.log(selectedGoalId);
+        //console.log(selectedGoalId);
         router.push({ pathname: '/GoalDetail', params: { selectedGoal: selectedGoalId } });
     };
 
@@ -161,35 +153,24 @@ const GoalList = () => {
     };
 
     const handleFilterPress = () => {
-        console.log('Icono de filtro presionado');
         setShowPopup(true);
         setShowBackdrop(true);
     };
 
-    const handleButtonPress = (option) => {
-        console.log("Botón presionado:", option);
-        setSelectedOption(option);
-        setShowPopup(false);
-    };
-
     const handleCancel = () => {
-        console.log('Cancelar');
         setShowPopup(false);
         setShowBackdrop(false);
     };
 
     const handleConfirm = () => {
-        console.log('Confirmar');
-    
         const formattedStartDate = startDate.toISOString();
     
         // Calcular la fecha final como 7 días después de la fecha de inicio
         const endDate = new Date(startDate);
         endDate.setDate(endDate.getDate() + 7);
-        const formattedEndDate = endDate.toISOString();
-    
+        /*const formattedEndDate = endDate.toISOString();
         console.log('Fecha de Inicio:', formattedStartDate);
-        console.log('Fecha Límite:', formattedEndDate);
+        console.log('Fecha Límite:', formattedEndDate); */
     
         filterGoalsByDateRange(startDate, endDate);
         setShowPopup(false);
@@ -200,14 +181,12 @@ const GoalList = () => {
         setShowStartDatePicker(false);
         if (selectedDate) {
             setStartDate(selectedDate);
-            console.log('Fecha de Inicio seleccionada:', selectedDate);
         }
     };
 
     const handleReset = () => {
-        setShowPopup(false); // Oculta el pop-up
+        setShowPopup(false);
         setGoals(originalGoals); // Restablece las metas a los valores originales
-        //console.log(originalGoals);
         setShowBackdrop(false);
     }
 
@@ -215,19 +194,17 @@ const GoalList = () => {
         if (patientId) {
             setLoading(true);
             try {
-                // Ajustar las fechas de inicio y fin para incluir la primera y última hora del día seleccionado
+                // Ajustar las fechas de inicio y fin para incluir la primera y última hora 
                 const adjustedStartDate = new Date(startDate);
-                adjustedStartDate.setHours(0, 0, 0, 0); // Establecer la primera hora del día
+                adjustedStartDate.setHours(0, 0, 0, 0); 
                 const adjustedEndDate = new Date(endDate);
-                adjustedEndDate.setHours(23, 59, 59, 999); // Establecer la última hora del día
+                adjustedEndDate.setHours(23, 59, 59, 999); 
     
                 // Filtrar las metas originales por las fechas ajustadas
                 const filteredGoals = originalGoals.filter(goal => {
                     const goalStartDate = new Date(goal.StartDate.toDate());
                     return goalStartDate >= adjustedStartDate && goalStartDate <= adjustedEndDate;
                 });
-    
-                console.log('Metas filtradas por fecha:', filteredGoals);
                 setGoals(filteredGoals);
             } catch (error) {
                 console.error("Error filtering goals:", error);
@@ -331,8 +308,8 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Ajusta la opacidad según lo desees
-        zIndex: 998, // Asegura que esté detrás del pop-up
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        zIndex: 998, 
     },
     popupContainer: {
         position: 'absolute',
@@ -367,8 +344,7 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.lightblue,
         padding: 10,
         borderRadius: 5,
-        marginTop: 10,
-        
+        marginTop: 10,    
     },
     buttonText: {
         color: Colors.white,
@@ -376,18 +352,18 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
     cancelButton: {
-        backgroundColor: Colors.red, // Puedes cambiar esto a tu color deseado
+        backgroundColor: Colors.red, 
         padding: 10,
         borderRadius: 5,
         marginTop: 10,
-        marginRight: 5, // Espacio adicional entre los botones
+        marginRight: 5, 
     },
     confirmButton: {
-        backgroundColor: Colors.green, // Puedes cambiar esto a tu color deseado
+        backgroundColor: Colors.green, 
         padding: 10,
         borderRadius: 5,
         marginTop: 10,
-        marginLeft: 5, // Espacio adicional entre los botones
+        marginLeft: 5, 
     },
     container: {
         flex: 1,
