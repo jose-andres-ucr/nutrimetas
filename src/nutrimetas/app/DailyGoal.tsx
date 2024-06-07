a7c5fe8d542928cd9cce3385a8616916a96d76a0
Gian <gfsopa@outlook.com>
2024-06-07 17:47:01 -0600

    Typo

--- src/nutrimetas/app/DailyGoal.tsx
+++ src/nutrimetas/app/DailyGoal.tsx
@@ -239,7 +239,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
     },
     textInput: {
-        backgroundColor: '#f0f0f0', // Fondo gris
+        backgroundColor: Colors.gray,
         borderColor: 'gray' ,
         borderWidth: 1,
         borderRadius: 5,
