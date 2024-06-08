447fe2c9e80ebb6d82bed29e3195fe8c29e96683
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-06-08 13:34:14 -0600

    Added config for build

---
+++ src/nutrimetas/app.config.js
@@ -0,0 +1,53 @@
+export default {
+    "expo": {
+      "name": "nutrimetas",
+      "slug": "nutrimetas",
+      "version": "1.0.0",
+      "orientation": "portrait",
+      "icon": "./assets/images/icon.png",
+      "scheme": "myapp",
+      "userInterfaceStyle": "automatic",
+      "splash": {
+        "image": "./assets/images/splash.png",
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
+      },
+      "assetBundlePatterns": [
+        "**/*"
+      ],
+      "ios": {
+        "supportsTablet": true
+      },
+      "android": {
+        "googleServicesFile": process.env.GOOGLE_SERVICES_JSON,
+        "package": "com.ucr.rnfirebase",
+        "adaptiveIcon": {
+          "foregroundImage": "./assets/images/adaptive-icon.png",
+          "backgroundColor": "#ffffff"
+        }
+      },
+      "web": {
+        "bundler": "metro",
+        "output": "static",
+        "favicon": "./assets/images/favicon.png"
+      },
+      "plugins": [
+        "expo-router",
+        "@react-native-firebase/app",
+        "@react-native-firebase/auth",
+        "expo-font"
+      ],
+      "experiments": {
+        "typedRoutes": true
+      },
+      "extra": {
+        "router": {
+          "origin": false
+        },
+        "eas": {
+          "projectId": "cb92d28c-5adf-4328-9398-09b18a36afb2"
+        }
+      }
+    }
+  }
+  
