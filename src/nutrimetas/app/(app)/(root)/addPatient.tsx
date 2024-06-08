c75d94046f58c801147fa5a1299755b182fc234c
FabianCalvoAlcazar <fabiianca16@gmail.com>
2024-06-07 19:27:02 -0600

    Relative path for addPatient

--- src/nutrimetas/app/(app)/(root)/addPatient.tsx
+++ src/nutrimetas/app/(app)/(root)/addPatient.tsx
@@ -10,7 +10,7 @@ import { StatusBar } from 'expo-status-bar';
 import firestore from '@react-native-firebase/firestore';
 import { showMessage } from "react-native-flash-message";
 import { useMutation } from "@tanstack/react-query"
-import loadingGif from '../assets/images/loading.gif'
+import loadingGif from '@/assets/images/loading.gif'
 
 import Colors from '@/constants/Colors';
 import { View } from "@/components/Themed";
