94611751e4abd105b94ad915f0a020cc238572f0
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-07-03 20:04:50 -0600

    Small refactor of login session types

--- shared/LoginSession.tsx
+++ shared/LoginSession.tsx
@@ -13,28 +13,31 @@ export const SessionDispatchContext =
 // User roles on the app
 export type UserRole = "professional" | "patient" | "admin";
 
-// User data on the DB
-export type UserData = {
+// Common user data on the DB
+type UserDataBase = {
     role: UserRole,
     docId: string,
     docContents: any,
 }
 
 // Patient data on the DB
-export type PatientData = UserData & {
+export type PatientData = UserDataBase & {
     role: "patient",
     assignedProfDocId: string,
 }
 
 // Professional data on the DB
-export type ProfessionalData = UserData & {
+export type ProfessionalData = UserDataBase & {
     role: "professional",
 }
 
 // Professional data on the DB
-export type AdminData = UserData & {
+export type AdminData = UserDataBase & {
     role: "admin",
 }
+
+// Possible user data
+export type UserData = AdminData | ProfessionalData | PatientData;
 
 // User session on the app
 export type LoginSession = {
