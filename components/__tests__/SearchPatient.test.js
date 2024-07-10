77da6f3d683657409ca1f543e9fefe03fd4ca2c2
Gian <gfsopa@outlook.com>
2024-07-10 10:32:31 -0600

    Added second test and fixe format

--- components/__tests__/SearchPatient.test.js
+++ components/__tests__/SearchPatient.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react-native';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import TransferPatient from '../../app/(app)/(root)/TransferPatient';
 import { SessionContext } from '@/shared/LoginSession';
 
@@ -11,7 +11,10 @@ jest.mock('expo-router', () => ({
 
 jest.mock('@/components/FetchData', () => ({
   usePatientsFirestoreQuery: jest.fn().mockReturnValue({
-    data: [{ idNumber: '12345678', firstName: 'Juan', lastName: 'Doe' }],
+    data: [
+      { idNumber: '12345678', firstName: 'Juan', lastName: 'Doe' },
+      { idNumber: '87654321', firstName: 'Jane', lastName: 'Smith' }
+    ],
     error: null,
     isLoading: false,
   }),
@@ -34,15 +37,22 @@ jest.mock('@/assets/images/greenCheckIcon.png', () => 'greenCheckIcon');
 jest.mock('@/assets/images/redCheckIcon.png', () => 'redCheckIcon');
 jest.mock('@/assets/images/searchIcon.png', () => 'searchIcon');
 
-// Prueba de renderizado
-it('renders TransferPatient component correctly', () => {
+// Prueba de búsqueda
+it('filters patients based on search input', () => {
   render(
     <SessionContext.Provider value={{ docId: 'current-id' }}>
       <TransferPatient />
     </SessionContext.Provider>
   );
 
-  expect(screen.getByText('Selecionar Pacientes A Transferir')).toBeTruthy();
-  expect(screen.getByPlaceholderText('Paciente')).toBeTruthy();
-  expect(screen.getByText('Transferir pacientes')).toBeTruthy();
+  // Verificar que ambos pacientes están inicialmente en la lista
+  expect(screen.getByText('Doe, Juan')).toBeTruthy();
+  expect(screen.getByText('Smith, Jane')).toBeTruthy();
+
+  // Simular entrada de búsqueda
+  fireEvent.changeText(screen.getByPlaceholderText('Paciente'), 'Jane');
+
+  // Verificar que solo el paciente que coincide con la búsqueda está en la lista
+  expect(screen.queryByText('Doe, Juan')).toBeNull();
+  expect(screen.getByText('Smith, Jane')).toBeTruthy();
 });
